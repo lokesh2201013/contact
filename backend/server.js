f13b73f41544ee54a2e3494fa3c337532e822799
lokesh2201013 <lokeshchoraria2@gmail.com>
2024-02-10 01:54:13 +0530

    nodejs

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const app= express();
+const dotenv=require("dotenv").config();
+const port=process.env.PORT || 5000;
+app.listen(port,()=>
+{
+    console.log("server listening on port ${port}");
+});
